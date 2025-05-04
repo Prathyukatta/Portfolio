fe5e94e318738fe56fe89d04ebd1e77ea9a8b320
Prathyukatta <prathyushakatta1@gmail.com>
2025-05-04 22:33:06 +0530

    Update portfolio content

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -79,38 +79,6 @@ export const Projects = () => {
                                 
                     
                     
-                                <div className="p-6 rounded-xl border-white/10 hover:translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-                        <h3 className="text-xl font-bold mb-2">
-                           <strong>Online e Voting  System</strong></h3>
-                            <p className="text-gray-400 mb-4 ">Developed a secure and user-friendly online e-Voting  </p>
-                                <p className="text-gray-400 mb-4 ">system to ensure transparent and efficient election processes.
-                                </p>
-                                <div className="flex flex-wrap gap-2 mb-4">
-                                    {["HTML", "CSS","JavaScript", "Bootstrap", "Python", "SQL"].map((tech, key) => (
-                                         <span 
-                                         key={key}
-                                         className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
-                                          hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
-             
-                                          {tech}
-                                          </span>
-
-                                    ))}
-                                </div>
-                                <div className="flex justify-between items-center">
-                                    <a href="https://right2vote.in/"
-                                    className=" py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
-                                          text-blue-400 hover:text-blue-300 transition-colors my-4 ">View Project 
-                                          </a>
-                                          <a href="https://github.com/Prathyukatta?tab=repositories"
-                                    className=" py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
-                                          text-blue-400 hover:text-blue-300 transition-colors my-4 ">GitHub 
-                                          </a>
-                                    
-                                   
-                                </div>
-                                </div>
-                            
 
             
         <div className="p-6 rounded-xl border-white/10 hover:translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
